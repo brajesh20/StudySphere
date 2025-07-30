96f0494544fd20c6ed65d93049f246c763b86c2f
Brajesh Kumar <brajeshkumarshiv@gmail.com>
2025-07-31 01:53:26 +0530

    removed secret files

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,16 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
-      "/api": {
-        target: "https://campusnotes-api.onrender.com",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    host: '0.0.0.0',
+    port: 5173,
   },
   plugins: [react(), tailwindcss()],
 });
